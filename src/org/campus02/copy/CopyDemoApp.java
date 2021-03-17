dabb43164f4dc5b9b4a2c184f11ef05c44594720
Stephan Kochauf <stephan.kochauf@lv.campus02.at>
2021-03-17 20:32:37 +0100

    CopyFile

---
+++ src/org/campus02/copy/CopyDemoApp.java
@@ -0,0 +1,12 @@
+package org.campus02.copy;
+
+public class CopyDemoApp {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static void copyFiles(String source, String destination){
+
+    }
+}
